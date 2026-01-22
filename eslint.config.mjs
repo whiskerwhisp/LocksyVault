ec8fcd62a3cfee45093e4ee793c53855ba211100
riya <riyarana1437@gmail.com>
2026-01-22 19:51:27 +0530

    new fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,22 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
+// // eslint-disable-next-line import/no-anonymous-default-export
+// export default {...eslintConfig, rules: {    "@typescript-eslint/no-explicit-any": "off"
+// }};
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,10 +28,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {...eslintConfig, rules: {    "@typescript-eslint/no-explicit-any": "off"
-}};
